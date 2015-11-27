ed12675c6fe75dee3fd4df9b300351b51b3d55ce
maggie-moheb <maggiemoheb@gmail.com>
2015-11-27 20:02:06 +0200

    adding profile and create post

--- app/src/main/java/com/bugs/bugs/CreatePost.java
+++ app/src/main/java/com/bugs/bugs/CreatePost.java
@@ -1,33 +1,24 @@
 package com.bugs.bugs;
-import android.os.Bundle;
+
 import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ImageView;
 
 
-public class Profile extends ActionBarActivity {
-    private ImageView mImageView;
+public class CreatePost extends ActionBarActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile);
-
-
-//bind imageview with your xml's id
-
-        mImageView = (ImageView)findViewById(R.id.imageView);
-
-//set resource for imageview
-
-        mImageView.setImageResource(R.drawable.profilepic);
+        setContentView(R.layout.activity_create_post);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_profile, menu);
+        getMenuInflater().inflate(R.menu.menu_create_post, menu);
         return true;
     }
 
